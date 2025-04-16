a1aca701c6e498d25d8d77dbbc50fe6c24066e81
volonte malisava <volontemuhi@gmail.com>
2025-04-16 10:23:54 +0200

    creation de code a tester dans une autre branche barbare

---
+++ script.js
@@ -0,0 +1,10 @@
+//fonction pour accuillir les visiteur
+const welcome = () => {
+    alert("Bienvenue sur mon site");
+}
+
+//FONCTION POUR CHANGER LA COULEUR DU TEXTE
+const changeColor = () => {
+    const text = document.getElementById("text");
+    text.style.color = "bred";
+}
